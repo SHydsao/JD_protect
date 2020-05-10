2fe8229e2c1c6a79257df90f30c2d9a68d7b3ebd
xhmbangbang <whghaigui@outlook.com>
2020-05-10 09:52:56 +0800

    这是首次提交的

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+let gulp = require("gulp");
+let htmlmin = require("gulp-htmlmin");
+let connect = require('gulp-connect');
+let sass = require("gulp-sass");
+let uglify = require("gulp-uglify");
+let babel = require('gulp-babel');
+let rename = require("gulp-rename");
+
+
+// 监听任务
+gulp.task("watchall", async ()=>{
+    //压缩html的    
+    gulp.watch("./src/jd/html/*.html",async ()=>{
+        gulp.src("./src/jd/html/*.html")
+        .pipe(htmlmin({
+            collapseWhitespace:true
+        }))
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\JD-PRO\\html"));
+    });
+    gulp.watch("./src/jd/images/**/*",async ()=>{
+        gulp.src("./src/jd/images/**/*")
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\JD-PRO\\images"));
+    });
+    gulp.watch("./src/jd/icon/**/*",async ()=>{
+        gulp.src("./src/jd/icon/**/*")
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\JD-PRO\\icon"));
+    });
+    //sass编译 
+    gulp.watch("./src/jd/css/*.scss",async ()=>{
+       gulp.src("./src/jd/css/*.scss")
+       .pipe(sass())
+       .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\JD-PRO\\css"));
+    });
+});   
+
